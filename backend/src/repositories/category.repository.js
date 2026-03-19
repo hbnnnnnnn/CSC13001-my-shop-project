3c032d20cd7a45bfabc00625342bd1c30a9b7ec2
Ngo Hong Thanh <thanhngo2707@gmail.com>
2026-03-20 00:48:19 +0700

    feat: implement category CRUD

--- backend/src/repositories/category.repository.js
+++ backend/src/repositories/category.repository.js
@@ -1,10 +1,20 @@
 // chú ý sql injection, dùng $1, $2, ... và pg-format để tránh
 const BaseRepository = require("./base.repository.js");
 const db = require("../config/db.js");
+const format = require("pg-format");
 
 class CategoryRepository extends BaseRepository {
     constructor() {
         super("category", "category_id", db);
+    }
+
+    async findByName(name) {
+        const query = format(
+            "SELECT * FROM %I WHERE name = $1",
+            this.tableName
+        );
+        const result = await this.db.query(query, [name]);
+        return result.rows[0];
     }
 }
 
